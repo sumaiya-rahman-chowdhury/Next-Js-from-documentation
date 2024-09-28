9b669b6d9087a87740d67c10bf542181b242189e
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2024-09-28 07:56:19 -0700

    linking and navigating

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from "next"
+import Link from "next/link"
 export const metadata: Metadata = {
   title: 'Next Js'
 }
 export default function Page() {
   // return <h1>Hello World</h1>
-  // return '...'
+  return <Link href="/dashboard">Dashboard</Link>
 }
