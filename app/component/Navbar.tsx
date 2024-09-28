91729a64c87848e9d00a4ca66dd38a3ebc21c9a8
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2024-09-28 07:02:22 -0700

    modify the title

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+   return ( 
+   <nav className="mt-10">
+    <ul className="flex justify-around items-center ">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+</nav>)
+}
+export default Navbar;
