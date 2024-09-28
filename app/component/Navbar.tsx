4fd0bcd0534bbd3754f14b515a4ab5796d08a48e
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2024-09-28 07:38:00 -0700

    active nav links

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,11 +1,23 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
+    const pathName = usePathname()
    return ( 
    <nav className="mt-10">
     <ul className="flex justify-around items-center ">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-    </ul>
+      <Link className={`link ${pathName === '/' ? 'active' : ''}`} href={"/"}>
+      Home
+      </Link>
+      <Link className={`link ${pathName === '/about' ? 'active':''}`} href={"/about"}>
+      About
+      </Link>
+      <Link className={`link ${pathName === '/contacts' ? 'active':''}`} href={"/contacts"}>
+      Contacts
+      </Link>
+    </ul> 
 </nav>)
 }
 export default Navbar;
